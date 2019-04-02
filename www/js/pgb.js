3a6362224c3fe0be2450c5e41c6c53313898b3a2
s-115-21 <s-115-21@gumiforest.ae.krakow.pl>
2019-04-02 15:59:07 +0200

    added personal network info buton

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -29,3 +29,18 @@ function personalInfo() {
 	navigator.notification.alert(info);
 	
 }
+function netInfo() {
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+
+    alert('Connection type: ' + states[networkState]);
+}
